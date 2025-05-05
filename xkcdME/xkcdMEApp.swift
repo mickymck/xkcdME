7c8d741107c78f8ae8ff4cde3658bbbbedda296e
Micky McKeon <mickymckeon@gmail.com>
2025-05-05 17:45:08 -0400

    Initial Commit

---
+++ xkcdME/xkcdMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  xkcdMEApp.swift
+//  xkcdME
+//
+//  Created by Micky McKeon on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct xkcdMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xkcdMETests/xkcdMETests.swift
@@ -0,0 +1,17 @@
+//
+//  xkcdMETests.swift
+//  xkcdMETests
+//
+//  Created by Micky McKeon on 5/5/25.
+//
+
+import Testing
+@testable import xkcdME
+
+struct xkcdMETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
