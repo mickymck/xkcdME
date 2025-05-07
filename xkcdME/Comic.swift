2db4b7ed2a789cc0cf14cc863fc075fcb5e2020c
Micky McKeon <mickymckeon@gmail.com>
2025-05-07 08:04:55 -0400

    Add Date and Date tests

--- xkcdME/Comic.swift
+++ xkcdME/Comic.swift
@@ -7,14 +7,37 @@
 
 import Foundation
 
-class Comic: Codable {
-    let num: Int
+class Comic: Decodable {
+    let number: Int
     let title: String
-    let img: String
+    let imageUrl: String
+    let date: Date?
     
-    init(num: Int, title: String, img: String) {
-        self.num = num
+    enum CodingKeys: String, CodingKey {
+        case number = "num"
+        case title
+        case imageUrl = "img"
+        case month
+        case day
+        case year
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        number = try container.decode(Int.self, forKey: .number)
+        title = try container.decode(String.self, forKey: .title)
+        imageUrl = try container.decode(String.self, forKey: .imageUrl)
+        
+        let month = try container.decode(String.self, forKey: .month)
+        let day = try container.decode(String.self, forKey: .day)
+        let year = try container.decode(String.self, forKey: .year)
+        self.date = ComicDate.create(month: month, day: day, year: year)
+    }
+    
+    init(number: Int, title: String, imageUrl: String, month: String, day: String, year: String) {
+        self.number = number
         self.title = title
-        self.img = img
+        self.imageUrl = imageUrl
+        self.date = ComicDate.create(month: month, day: day, year: year)
     }
 }
